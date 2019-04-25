bdf157c903406b52a4d04492499f4b44ec0dfc82
Microclimate User <microclimate.user@localhost>
2019-04-25 14:15:38 +0000

    test

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = Promise;
 class App {
   public express: express.Application;
+  console.log(process.env);
   public mongoUrl: string = 'mongodb://'+process.env.MONGOUSERNAME+':'+process.env.MONGOPASSWORD+'@'+process.env.HOSTNAMEMONGODB+':'+process.env.MONGOPORT+'/admin';
   constructor() {
     this.express = express();
